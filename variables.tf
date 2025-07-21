ef63fa1b1fd4fade52e2ddfbad88545800012444
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2025-07-21 19:52:27 +0530

    feat: fixing the ipv6 issue

--- variables.tf
+++ variables.tf
@@ -157,5 +157,5 @@ variable "ipv6_access_type" {
 variable "stack_type" {
   description = "The stack type of the subnetwork. Use IPV4_ONLY or IPV4_IPV6 when using IPv6."
   type        = string
-  default     = ""
+  default     = "IPV4_IPV6"
 }
